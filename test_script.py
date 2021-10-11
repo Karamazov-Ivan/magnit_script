ded935404ca87022c944533700a226fe716f5104
Karamazov_Ivan <oleg13.troshin@gmail.com>
2021-10-11 22:46:38 +0300

    v0.6

--- test_script.py
+++ test_script.py
@@ -8,24 +8,38 @@ directory = settings.PATH
 # Сохраняем имена всех файлов в директории
 files = os.listdir(directory) 
 
+new_folder = settings.PATH + "/Готовые файлы"
+
+if "Готовые файлы" in files:
+    inp = input("Папка 'Готовые файлы' уже существует. Перезаписать?\ny/n\n")
+    if inp == "y":
+        pass
+    elif inp == "n":
+        exit()
+
+if "Готовые файлы" not in files:
+    # Создаём новую папку
+    os.mkdir(new_folder)   
+
 for file_name in files:
-    # Открываем файл
-    wb = openpyxl.reader.excel.load_workbook(filename=directory + '/' + file_name)
+    if file_name.endswith(".xlsx"):
+        # Открываем файл
+        wb = openpyxl.reader.excel.load_workbook(filename=directory + '/' + file_name)
 
-    # Назначаем активным первый Лист
-    wb.active = 0
+        # Назначаем активным первый Лист
+        wb.active = 0
 
-    # Сохраняем в переменную активный лист
-    sheet = wb.active
+        # Сохраняем в переменную активный лист
+        sheet = wb.active
 
-    sheet['D1'].value = "Объект"
+        sheet['D1'].value = "Объект"
 
-    for str_count in range(2, 16):
-        sheet[f'D{str_count}'].value = file_name[:-5]
+        for str_count in range(2, 16):
+            sheet[f'D{str_count}'].value = file_name[:-5]
         
-    # Сохраняем файл
-    wb.save(settings.SAVE_PATH + '/' + file_name)
-    
-    print(f"Файл {file_name[:-5]} перезаписан!")
+        # Сохраняем файл
+        wb.save(str(new_folder) + '/' + file_name)
+        
+        print(f"Файл '{file_name[:-5]}' перезаписан!")
 
 print("Всё сработало, расходимся...")
