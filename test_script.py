200d5a1c28eeb10f8c27ea542b4191302dac365f
Karamazov_Ivan <oleg13.troshin@gmail.com>
2021-10-11 21:21:41 +0300

    v0.0003

--- test_script.py
+++ test_script.py
@@ -2,22 +2,23 @@ import os
 import settings
 import openpyxl
 
+# Обращаемся к файлу настроек
 directory = settings.PATH
 
+# Берём директорию директорию
 files = os.listdir(directory) 
 
+# Сохраняем в переменную сконкатенированные директорию и название файла 
 wb = openpyxl.reader.excel.load_workbook(filename=directory + '/' + files[0])
 
+# Назначаем активным первый Лист
 wb.active = 0
 
+# Сохраняем в переменную активный лист
 sheet = wb.active
 
-print(sheet['D1'].value)
 sheet['D1'].value = "Объект"
 
-print(sheet['D1'].value)
 wb.save(settings.SAVE_PATH + '/' + files[0])
 
 
-print(sheet['A1'].value)
-print(files[0])
